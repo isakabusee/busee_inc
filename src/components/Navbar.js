ecded2e0fc8e86cf37b2e7336e7a3f7590ee4278
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-04-27 00:37:51 -0700

    more styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,18 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const  Navbar = () => {
   return (
-    <div>
+
    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
      <div className="title-name"><h2>KukuPay</h2></div>
   <a className="navbar-brand" href="#"></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
   </button>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link"  href="/">Home <span className="sr-only"></span></a>
+        <a className="nav-link active"  href="/">Home <span className="sr-only"></span></a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="/info">Business Model</a>
@@ -41,7 +41,7 @@ const  Navbar = () => {
 
   </div>
 </nav>
-    </div>
+
   )
 }
  export default Navbar;
